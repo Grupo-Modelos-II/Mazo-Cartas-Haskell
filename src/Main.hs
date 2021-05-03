12ab360d5943db4c8a9a229c65c665a431ee12aa
Jesús Bermúdez <jema0622@outlook.com>
2021-05-03 13:51:51 -0500

    Funcion Main por parametros de teclado

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,20 @@
-import Game
+import Operations
 
-main = putStr(hola)
+main = do
+ input <- getLine
+ let response = read input ::Int
+
+ if response == 1 then do
+  value <- getLine
+  symbol <- getLine
+  print(pedirCarta(value, symbol))
+ else if response == 2 then do
+  value <- getLine
+  symbol <- getLine
+  print(buscarCarta(value, symbol) getMallet)
+ else do
+  symbol <- getLine
+  print(preguntarValor symbol mallet)
+ putStr "Repetir Proceso? Hijo de tu chingada Madre >:V"
+ input <- getLine
+ if input == "yes" || input == "y" then main else putStr "Chauuu"
