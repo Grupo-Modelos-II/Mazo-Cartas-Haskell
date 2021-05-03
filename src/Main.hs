b8ab19676b6a7d18ce478ed1d31b40d450dc2fa0
Jesús Bermúdez <jema0622@outlook.com>
2021-05-03 10:27:36 -0500

    Initial Commit

---
+++ src/Main.hs
@@ -0,0 +1,3 @@
+import modules.Game
+
+main = putStr(hola)
---
+++ src/modules/Game.hs
@@ -0,0 +1,9 @@
+module Game where
+type Value = Int
+type Symbol = String
+
+type Card = (Value, Symbol)
+
+type Mallet = [Card]
+
+hola = "Llamada funcion"
